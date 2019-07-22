faf143fcc7ee242ab72e6f1090471aa8b8ea680a
Chris Hanagan <1239556090@mil>
2019-07-22 16:44:53 -0400

    Up to section 2.4.6 - unexpected bug

--- app/renderer.js
+++ app/renderer.js
@@ -17,11 +17,20 @@ newLinkForm.addEventListener('submit', (event) => {
   const url = newLinkUrl.value;
 
   fetch(url)
+    .then(validateResponse)
     .then(response => response.text())
     .then(parseResponse)
-    .then(findTitle);
+    .then(findTitle)
+    .then(title => storeLink(title, url))
+    .then(clearForm)
+    .then(renderLinks)
+    .catch(error => handleError(error, url));
 });
 
+clearStorageButton.addEventListener('click', () => {
+  localStorage.clear();
+  linksSection.innerHTML = '';
+});
 
 const clearForm = () => {
   newLinkUrl.value = null;
@@ -33,3 +42,43 @@ const parseResponse = (text) => {
 const findTitle = (nodes) => {
   return nodes.querySelector('title').innerText;
 }
+
+const storeLink = (title, url) => {
+  localStorage.setItem(url, JSON.stringify({ title: title, url: url }));
+}
+
+const getLinks = () => {
+  return Object.keys(localStorage)
+    .map(key => JSON.parse(localStorage.getItem(key)));
+}
+
+const handleError = (error, url) => {
+  errorMessage.innerHTML = `
+    There was an issue adding "${url}": ${error.message}
+  `.trim();
+  setTimeout(() => errorMessage.innerText = null, 5000);
+}
+
+const convertToElement = (link) => {
+  return `
+  <div class="link">
+  <h3>${link.title}</h3>
+  <p>
+  <a href="${link.url}">${link.url}</a>
+  </p>
+  </div>
+  `;
+}
+
+const validateResponse = (response) => {
+  if (response.ok) { return response }
+  throw new Error(`Status code of ${response.status}
+  ${response.statusText}`);
+}
+
+const renderLinks = () => {
+  const linkElements = getLinks().map(convertToElement).join('');
+  linksSection.innerHTML = linkElements;
+}
+
+renderLinks();
