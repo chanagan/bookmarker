9ad77eba5258e76ba85402020a3fb75d99f75870
chris.hanagan <chris.hanagan@gmail.com>
2019-10-30 16:55:14 -0400

    End of 2.4.1

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,24 @@
+const linksSection = document.querySelector('.links');
+const errorMessage = document.querySelector('.error-message');
+const newLinkForm = document.querySelector('.new-link-form');
+const newLinkUrl = document.querySelector('.new-link-url');
+const newLinkSubmit = document.querySelector('.new-link-submit');
+const clearStorageButton = document.querySelector('.clear-storage');
 
-    const button = document.querySelector('.alert');
-  
-  button.addEventListener('click', () => {
-        alert(__dirname)
-    });
+const button = document.querySelector('.alert');
+
+const clearForm= () => {
+    newLinkUrl.value = null;
+};
+
+button.addEventListener('click', () => {
+    alert(__dirname)
+});
+
+newLinkUrl.addEventListener('keyup', () => {
+    newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+newLinkForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const  url = newLinkUrl.value;
+});
